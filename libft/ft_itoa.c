68fadea0ba97c54e208e0cc834e58a45c48f5830
NineSuper <45767648+NineSuper@users.noreply.github.com>
2023-02-10 15:44:06 +0100

    new files

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -3,44 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tde-los- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/09 15:31:23 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/02/09 21:07:59 by tde-los-         ###   ########.fr       */
+/*   Created: 2023/02/10 08:55:46 by tde-los-          #+#    #+#             */
+/*   Updated: 2023/02/10 10:48:15 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_itoa(int n)
+static char	*ft_strrev(char *str, int i, int n, int signe)
 {
-    char    *str;
-    int     signe;
-    int     len;
-    int     i;
+	while (i >= 0)
+	{
+		str[i] = (signe * (n % 10)) + 48;
+		n = n / 10;
+		i--;
+	}
+	if (signe == -1)
+		str[0] = '-';
+	return (str);
+}
 
-    signe = 1;
-    len = 1;
-    i = n;
-    if (n < 0)
-    {
-        signe = -1;
-        len = 2;
-    }
-    while ((i = i / 10))
-        len++;
-    str = malloc(sizeof(char) * (len + 1));
-    if (str == NULL)
-        return (NULL);
-    str[len] = '\0';
-    i = len - 1;
-    while (i >= 0)
-    {
-        str[i] = (signe * (n % 10)) + 48;
-        n = n / 10;
-        i--;
-    }
-    if (signe == -1)
-        str[0] = '-';
-    return (str);
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		i;
+	int		signe;
+	int		len;
+
+	signe = 1;
+	len = 1;
+	i = n;
+	if (n < 0)
+	{
+		signe = -1;
+		len = 2;
+	}
+	while (i / 10 != 0)
+	{
+		len++;
+		i /= 10;
+	}
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+	i = len - 1;
+	str = ft_strrev(str, i, n, signe);
+	return (str);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 09:35:08 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/02/07 18:34:50 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/02/10 12:17:03 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,10 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	**ft_split(char const *s, char c);
 
 #endif
